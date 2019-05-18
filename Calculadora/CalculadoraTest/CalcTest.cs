eb504d66d4bf981a056adad646a7af2f54cf9e93
unknown <nieva_adrian@hotmail.com>
2019-05-18 20:50:43 +0200

    fist commit

---
+++ Calculadora/CalculadoraTest/CalcTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalculadoraTest
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        [Test]
+        public void prueba()
+        {
+            Assert.Pass();
+        }
+    }
+}
