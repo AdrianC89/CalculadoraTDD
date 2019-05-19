8664e47bb46008cccdf1c52403839971222610f0
unknown <nieva_adrian@hotmail.com>
2019-05-19 13:33:30 +0200

    Implementada Raiz Cuadrada

--- Calculadora/Calculadora/Calculadora.cs
+++ Calculadora/Calculadora/Calculadora.cs
@@ -10,6 +10,12 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            float valor = 120;
+            float resultadoEsperado = 10.9544511F;
+            float resultadoCalculado;
+
+            //Act
+            resultadoCalculado = Calculadora.Calc.Sqrt(valor);
         }
 
         public static int Suma(int a, int b)
@@ -36,9 +42,42 @@ namespace Calculadora
             return a/b;
         }
 
-        public static float Sqrt(float valor)
+        public static float Sqrt(float x)
         {
-            return 0;
+            //Raiz cuadrada a partir de la aproximación de Bakhshali
+            float n;
+            bool encontrado= false;
+            float mejorValor = 0;
+            float mejorDiferencia = 999999;
+            float diferencia = 999999;
+            while (!encontrado)
+            {
+                for(int i = 0; i < x; i++)
+                {
+                    diferencia = x - i * i;
+                    if (diferencia < 0)
+                    {
+                        diferencia *= -1;
+                    }
+                    if(diferencia < mejorDiferencia)
+                    {
+                        mejorValor = i;
+                        mejorDiferencia = diferencia;
+                    }
+
+                    if(diferencia > mejorDiferencia)
+                    {
+                        encontrado = true;
+                        break;
+                    }
+                }
+            }
+            n = mejorValor;
+
+            //no utilizamos las fuciones de calculadora porque resultaría una fórmula muy compleja de llamadas anidadas, menos intuitiva visualmente
+            float r = (n * n * n * n + 6 * n * n * x + x * x) / (4 * n * n * n + 4 * n * x);
+            Console.WriteLine(r);
+            return r;
         }
     }
 }
